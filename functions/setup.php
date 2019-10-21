d76383dc450f01942f01234843efe7fab64e3a31
Shubham Mathur <itseasy21@gmail.com>
2019-10-21 23:48:14 +0530

    Update gallery support

--- functions/setup.php
+++ functions/setup.php
@@ -92,5 +92,7 @@ add_action( 'after_setup_theme', 'woocommerce_support' );
 	if ( ! function_exists( 'woocommerce_support' ) ) {
 	function woocommerce_support() {
 	   add_theme_support( 'woocommerce' );
+		 add_theme_support( 'wc-product-gallery-zoom' );
+		 add_theme_support( 'wc-product-gallery-lightbox' );
 	}
 }
