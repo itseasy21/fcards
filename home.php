a62ceeebff339aac2378678554c7b93118a07ed5
Shubham Mathur <itseasy21@gmail.com>
2019-10-30 23:01:10 +0530

    Dynamic Home Content from Admin Panel

--- home.php
+++ home.php
@@ -40,33 +40,15 @@
          <div class="row">
            <?php get_sidebar(); ?>
             <div class="col-sm-8">
-               <h3 class="text-center heding_color font-weight-bolder heding_border mt-2 mb-2">WELCOME TO FOOTBALL CARDSDIRECT</h3>
-               <p class="text-justify">At football Cards Direct you are in the ideal place to find football cards and stickers from both here in
-                  the UK and from all around the world. As a full time business it enables us to provide customers with the
-                  professional and reliable service you would expect from the top match Attax. Adrenalyn XL and Panini
-                  sticker suppliers on the net. As collectors since hildhood (many years ago now) we know what collectors
-                  want and are happy to provide that service with the knowledge of man years of experience. Some promise
-                  much and are gone tomorrow, we’re here for the long term.
-               </p>
-               <p class="text-justify">At football Cards Direct you are in the ideal place to find football cards and stickers from both here in
-                  the UK and from all around the world. As a full time business it enables us to provide customers with the
-                  professional and reliable service you would expect from the top match Attax. Adrenalyn XL and Panini
-                  sticker suppliers on the net. As collectors since hildhood (many years ago now) we know what collectors
-                  want and are happy to provide that service with the knowledge of man years of experience. Some promise
-                  much and are gone tomorrow, we’re here for the long term.
-               </p>
-               <p class="text-justify">At football Cards Direct you are in the ideal place to find football cards and stickers from both here in
-                  the UK and from all around the world. As a full time business it enables us to provide customers with the
-                  professional and reliable service you would expect from the top match Attax. Adrenalyn XL and Panini
-                  sticker suppliers on the net. As collectors since hildhood (many years ago now) we know what collectors
-                  want and are happy to provide that service with the knowledge of man years of experience. Some promise
-                  much and are gone tomorrow, we’re here for the long term.
-               </p>
-               <p class="text-justify">
-                  THE HOME OF FOOTBALL CARDS, COLLECTINS FROM THE UK INCLUDING, MATCH ATTAX, ADDRENLYN XL,
-                  PANINI STICKERS WITH SO MUCH MORE FROM OUTSIDE THE UK INCLUDING
-                  SPAIN, FRANCE, ITALY, GERMANY AND MUCH MORE
-               </p>
+              <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                    <?php the_content()?>
+                    <?php wp_link_pages(); ?>
+              <?php
+                endwhile;
+                else :
+                  get_template_part('loops/404');
+                endif;
+              ?>
             </div>
          </div>
       </div>
