71e973bd88507b485fc26953a8ec0386395c8a0a
Shubham Mathur <itseasy21@gmail.com>
2019-11-01 23:05:43 +0530

    center banner

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     get_header();
     // b4st_main_before();
 ?>
-<div class="mb-5">
+<div class="mb-5 text-center">
   <a href="#">
     <img src="<?php echo get_template_directory_uri(); ?>/theme/img/banner.png" alt="banner_img" class="img-fluid img_banner"/>
   </a>
