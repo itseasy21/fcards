37026a3ea63b6aaf2e12e4e817b4418ee366425c
Shubham Mathur <itseasy21@gmail.com>
2019-11-01 23:03:16 +0530

    center banner

--- header.php
+++ header.php
@@ -13,8 +13,8 @@
 
   <div class="contaner1">
      <div class="row">
-        <div class="col-sm-4 bg-white">
-    <img src="<?php echo get_template_directory_uri(); ?>/theme/img/logo1.jpg" alt="logo_img" style="width: 96%;" class="mx-auto d-block" />
+        <div class="col-sm-4 bg-white text-center">
+          <img src="<?php echo get_template_directory_uri(); ?>/theme/img/logo1.jpg" alt="logo_img" style="width: 96%;" class="mx-auto d-block" />
         </div>
         <div class="col-sm-8 bg-white">
   <div class="row" style="margin: 12px;">
